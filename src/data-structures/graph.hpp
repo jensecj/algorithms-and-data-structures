1f334f5cd6586d9764e87e0816cfb10485584d50
Jens <jensecj@gmail.com>
2017-12-25 15:18:50 +0100

    added a (weighted) graph, using adjacency list or matrix.

---
+++ src/data-structures/graph.hpp
@@ -0,0 +1,147 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+/*
+  A graph structure.
+  Can use either an adjacency list or an adjacency matrix as a
+  representation for edges between nodes.
+*/
+
+#include <functional>
+#include <memory>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+namespace dads::graphs {
+
+class node_store {
+ public:
+  virtual void add_edge(int u, int v, int w) = 0;
+  virtual std::vector<int> nodes() = 0;
+  virtual std::vector<int> neighbours(int n) = 0;
+  virtual int weight(int u, int v) = 0;
+};
+
+class adjacency_list : public node_store {
+ private:
+  std::unordered_map<int, std::unordered_map<int, int>> list;
+
+ public:
+  void add_edge(int u, int v, int weight) override { list[u][v] = weight; }
+
+  std::vector<int> nodes() override {
+    std::vector<int> ns;
+    for (auto kvp : list) {
+      ns.push_back(std::get<0>(kvp));
+    }
+
+    return ns;
+  }
+
+  std::vector<int> neighbours(int n) override {
+    std::vector<int> ns;
+    for (auto kvp : list[n]) {
+      ns.push_back(std::get<0>(kvp));
+    }
+
+    return ns;
+  }
+
+  int weight(int u, int v) override { return list[u][v]; }
+};
+
+template <const std::size_t N>
+class adjacency_matrix : public node_store {
+ private:
+  std::vector<std::vector<int>> matrix;
+
+ public:
+  adjacency_matrix() : matrix(N, std::vector<int>(N, -1)) {}
+
+  void add_edge(int u, int v, int weight) override { matrix[u][v] = weight; }
+
+  std::vector<int> nodes() {
+    std::vector<int> ns;
+
+    for (std::size_t i = 0; i < N; i++) {
+      for (std::size_t j = 0; j < N; j++) {
+        // TODO: handle negative weight edges
+        if (matrix[i][j] > -1) {
+          ns.push_back(i);
+          break;
+        }
+      }
+    }
+
+    return ns;
+  }
+
+  std::vector<int> neighbours(int n) override {
+    std::vector<int> ns;
+
+    for (std::size_t i = 0; i < N; i++) {
+      // TODO: handle negative weight edges
+      if (matrix[n][i] > -1) {
+        ns.push_back(i);
+      }
+    }
+
+    return ns;
+  }
+
+  int weight(int u, int v) override { return matrix[u][v]; }
+};
+
+template <typename T>
+class graph {
+ private:
+  std::unique_ptr<T> _nodes;
+
+  graph() : _nodes(std::move(std::make_unique<T>())){};
+
+  graph(const graph &) = delete;
+  graph &operator=(const graph &) = delete;
+  graph(graph &&) = delete;
+  graph &operator=(graph &&) = delete;
+
+ public:
+  static std::unique_ptr<graph<T>> make() {
+    return std::unique_ptr<graph<T>>(new graph<T>());
+  }
+
+  void add_edge(int u, int v, int weight);
+  void add_bi_edge(int u, int v, int weight);
+  std::vector<int> nodes();
+  std::vector<int> neighbours(int n);
+  int weight(int u, int v);
+};
+
+template <typename T>
+void graph<T>::add_edge(int u, int v, int weight) {
+  _nodes->add_edge(u, v, weight);
+}
+
+template <typename T>
+void graph<T>::add_bi_edge(int u, int v, int weight) {
+  _nodes->add_edge(u, v, weight);
+  _nodes->add_edge(v, u, weight);
+}
+
+template <typename T>
+std::vector<int> graph<T>::nodes() {
+  return _nodes->nodes();
+}
+
+template <typename T>
+std::vector<int> graph<T>::neighbours(int n) {
+  return _nodes->neighbours(n);
+}
+
+template <typename T>
+int graph<T>::weight(int u, int v) {
+  return _nodes->weight(u, v);
+}
+
+}  // namespace dads::graphs
+
+#endif
---
+++ tests/data-structures/graph.test.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <random>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include <data-structures/graph.hpp>
+
+using dads::graphs::adjacency_list;
+using dads::graphs::adjacency_matrix;
+using dads::graphs::graph;
+
+namespace {
+
+class Graph_AdjacencyList : public ::testing::Test {
+ protected:
+  std::unique_ptr<graph<adjacency_list>> G;
+  void SetUp() override { G = graph<adjacency_list>::make(); }
+  void TearDown() override { G.reset(); }
+};
+
+TEST_F(Graph_AdjacencyList, CanAddEdge) {  // NOLINT
+  G->add_edge(0, 1, 5);
+
+  int u = G->neighbours(0)[0];
+
+  ASSERT_EQ(u, 1);
+  ASSERT_EQ(G->weight(0, u), 5);
+}
+
+TEST_F(Graph_AdjacencyList, CanAddBiEdge) {  // NOLINT
+  G->add_bi_edge(0, 1, 5);
+
+  int u = G->neighbours(0)[0];
+  ASSERT_EQ(u, 1);
+  ASSERT_EQ(G->weight(0, u), 5);
+
+  int v = G->neighbours(1)[0];
+  ASSERT_EQ(v, 0);
+  ASSERT_EQ(G->weight(1, v), 5);
+}
+
+TEST_F(Graph_AdjacencyList, CanGetNodes) {  // NOLINT
+  G->add_bi_edge(0, 1, 10);
+  G->add_bi_edge(1, 2, 10);
+  G->add_bi_edge(0, 2, 10);
+
+  std::vector<int> v = G->nodes();
+  std::sort(std::begin(v), std::end(v));
+
+  ASSERT_EQ(v[0], 0);
+  ASSERT_EQ(v[1], 1);
+  ASSERT_EQ(v[2], 2);
+}
+
+typedef adjacency_matrix<10> adj_matrix;
+class Graph_AdjacencyMatrix : public ::testing::Test {
+ protected:
+  std::unique_ptr<graph<adj_matrix>> G;
+  void SetUp() override { G = graph<adj_matrix>::make(); }
+  void TearDown() override { G.reset(); }
+};
+
+TEST_F(Graph_AdjacencyMatrix, CanAddEdge) {  // NOLINT
+  G->add_edge(0, 1, 5);
+
+  int u = G->neighbours(0)[0];
+
+  ASSERT_EQ(u, 1);
+  ASSERT_EQ(G->weight(0, u), 5);
+}
+
+TEST_F(Graph_AdjacencyMatrix, CanAddBiEdge) {  // NOLINT
+  G->add_bi_edge(0, 1, 5);
+
+  int u = G->neighbours(0)[0];
+  ASSERT_EQ(u, 1);
+  ASSERT_EQ(G->weight(0, u), 5);
+
+  int v = G->neighbours(1)[0];
+  ASSERT_EQ(v, 0);
+  ASSERT_EQ(G->weight(1, v), 5);
+}
+
+TEST_F(Graph_AdjacencyMatrix, CanGetNodes) {  // NOLINT
+  G->add_bi_edge(0, 1, 10);
+  G->add_bi_edge(1, 2, 10);
+  G->add_bi_edge(0, 2, 10);
+
+  std::vector<int> v = G->nodes();
+  std::sort(std::begin(v), std::end(v));
+
+  ASSERT_EQ(v[0], 0);
+  ASSERT_EQ(v[1], 1);
+  ASSERT_EQ(v[2], 2);
+}
+
+}  // namespace
